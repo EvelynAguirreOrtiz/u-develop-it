8fa8c47b1225f52b19e9fe25fb6b2e255964b784
Lash Blast <evelyn.aguirre.9895@gmail.com>
2022-04-18 13:18:28 -0500

    retrieve candidate information

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const mysql = require('mysql2');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -7,6 +8,18 @@ const app = express();
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
+// Connect to database
+const db = mysql.createConnection(
+  {
+    host: 'localhost',
+    // Your MySQL username,
+    user: 'root',
+    // Your MySQL password
+    password: 'H&NsTi0SeEzUR',
+    database: 'election'
+  },
+  console.log('Connected to the election database.')
+);
 
 // GET test route 
 // always delete
@@ -16,12 +29,15 @@ app.get('/', (req, res) => {
   });
 });
 
+db.query(`SELECT * FROM candidates`, (err, rows) => {
+  console.log(rows);
+});
+
 // always at end before start Express.js
 // Default response for any other request (Not Found)
 app.use((req, res) => {
   res.status(404).end();
 });
-
 // start Express.js
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
